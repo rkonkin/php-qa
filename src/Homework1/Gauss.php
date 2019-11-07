f6b8b178c513d17dad0d27bff6dfca4a067811c3
Roman Konkin <rkonkin@avito.ru>
2019-11-07 23:00:00 +0300

    complete Gauss

--- src/Homework1/Gauss.php
+++ src/Homework1/Gauss.php
@@ -12,9 +12,17 @@ class Gauss {
 
         return $sum;
     }
+
+
+    public function get2(int $number): int {
+        return $number * ($number + 1) / 2;
+    }
 }
 
 $sum = new Gauss();
 
 var_dump($sum->get(3));
 var_dump($sum->get(100));
+
+var_dump($sum->get2(3));
+var_dump($sum->get2(100));
